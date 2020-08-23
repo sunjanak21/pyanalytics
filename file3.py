518e87bbca5794975cc29b143086554a8b629f32
sunjanak21 <70102675+sunjanak21@users.noreply.github.com>
2020-08-23 22:44:09 +0530

    adf

---
+++ file3.py
@@ -0,0 +1,14 @@
+ist1 = [1,2,3,4,5,'a','Dhiraj',"Upadhyaya",True]  #list type of object with data
+list1  #print when through spyder
+type(list1)  #type of object
+print(list1)  #print when running complete file
+list1
+sorted([1,5,3,2,4,5,324,43,4,4,3,23,434,53442,244,2245224,523])
+list2 = ['f','c','d','e','f']
+list2
+list2.count('f')
+len(list2)
+dir(list)
+dir(tuple)
+dir(list) #functions which can# -*- coding: utf-8 -*-
+
